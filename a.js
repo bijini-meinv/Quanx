d2213d3a8e835462b89c7dc54cf3dfe4748314a3
x <98009750+bijini-meinv@users.noreply.github.com>
2023-03-06 21:50:44 +0800

    Update a.js

--- a.js
+++ a.js
@@ -5,7 +5,7 @@ const BASE_URL_GPT = 'https://chat.openai.com/'
 const Region_URL_GPT = 'https://chat.openai.com/cdn-cgi/trace'
 
 const link = { "media-url": "https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/img/southpark/7.png" } 
-const policy_name = "Netflix" //填入你的 netflix 策略组名
+const policy_name = "自动选择" //填入你的 netflix 策略组名
 
 const arrow = " ➟ "
 
